a4e7f962dfdddcd9efda2922d3c194fab10f65e9
lostsquirrel <songli1988@gmail.com>
2015-03-23 18:26:08 +0800

    add test alipay

--- test/script/test_pay.py
+++ test/script/test_pay.py
@@ -43,4 +43,21 @@ def test_body():
         print "HTTP STATUS CODE: %s" % rep.status_code
         print rep.content.decode('unicode-escape')
         
-test_body()
+def test_alipay_notify():
+    url = '%s/pay_notify/alipay' % apiHost
+    params = {'seller_email': ['329005952@qq.com'], 'sign': ['phwWlhdrAP2WiSHb4r1imM7SwY+3tZP/BRfztujogm4ThGyrLjM4EGcMBJ8+rPUXwZ6nLJVq2gLCNqMq9hEkFzstcjcUqDP+znzszfxiuUlhrTOJqDU23Jy/HaOvXa8EkkqhwlbLQlL8FJTIMyBDQ9zEnZeE/E/8BLpdrk80PqU='], 'subject': ['\xe7\xbe\x8e\xe7\x9d\xab\xe4\xba\xa7\xe5\x93\x81001'], 'is_total_fee_adjust': ['N'], 'gmt_create': ['2015-03-23 17:05:07'], 'out_trade_no': ['1427101485118137'], 'sign_type': ['RSA'], 'body': ['\xe7\xbe\x8e\xe7\x9d\xab\xe4\xba\xa7\xe5\x93\x81001'], 'price': ['0.01'], 'buyer_email': ['tang.ke@me.com'], 'discount': ['0.00'], 'trade_status': ['TRADE_SUCCESS'], 'gmt_payment': ['2015-03-23 17:05:08'], 'trade_no': ['2015032300001000170047235055'], 'seller_id': ['2088812906941947'], 'use_coupon': ['N'], 'payment_type': ['1'], 'total_fee': ['0.01'], 'notify_time': ['2015-03-23 17:19:26'], 'quantity': ['1'], 'notify_id': ['3c07ebe0825cb232ffabb95a8c46d1d92y'], 'notify_type': ['trade_status_sync'], 'buyer_id': ['2088002033090175']}
+    rep = requests.post(url, data=params)
+    if rep.status_code == 200:
+        try:
+            data = rep.json()
+            json.dumps(data, sort_keys=True, indent=4, separators=(',', ': ')).decode('unicode-escape')
+        except:
+            print rep.content.decode('unicode-escape')
+    else :
+        print "HTTP STATUS CODE: %s" % rep.status_code
+        print rep.content.decode('unicode-escape')
+        
+if __name__ == '__main__':
+    test_alipay_notify()
+    pass
+# test_body()
